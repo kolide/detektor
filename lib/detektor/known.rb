19604684a3dfeceef7d833d0fd95f5056c8b0f28
Phoenix Rodden <phoenix.rodden@agilebits.com>
2025-10-17 12:21:46 -0400

    fix nil?

--- lib/detektor/known.rb
+++ lib/detektor/known.rb
@@ -4,11 +4,15 @@ module Detektor
   module Known
     module Os
       def self.from(str)
+        return nil if str.nil?
+
         existing = constants.find do |os|
           os.name.casecmp?(str)
         end
 
-        const_get(existing) || Result::Os[str]
+        return const_get(existing) unless existing.nil?
+
+        Result::Os[str]
       end
     end
   end
