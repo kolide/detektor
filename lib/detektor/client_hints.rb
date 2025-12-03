ff36d2bd393757d44bfc6d760d99c915f7ff7d99
Phoenix Rodden <phoenix.rodden@agilebits.com>
2025-12-03 13:02:49 -0500

    Standard

--- lib/detektor/client_hints.rb
+++ lib/detektor/client_hints.rb
@@ -3,16 +3,14 @@
 require_relative "client_hints/constants"
 require_relative "client_hints/parser"
 require_relative "client_hints/response_builder"
-#require_relative "result"
+# require_relative "result"
 module Detektor
   module ClientHints
     def self.detect(headers, what = nil)
       # while we _can_ restrict which headers we process to those
       # necessary for determining `what`, we might as well process
       # all of them, because there are not that many
-      parsed = parse_headers(headers)
-
-      parsed
+      parse_headers(headers)
 
       # result = Result.new
       # result.ch_result = parsed
